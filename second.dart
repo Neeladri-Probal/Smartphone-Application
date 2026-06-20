58384a3508132b99493080e6ba8c86aa7cbd4ed9
Neeladri Probal <neeladriprobal45@gmail.com>
2026-06-20 11:10:44 +0600

    type conversion, list, map, runes, runtimetype, var

---
+++ second.dart
@@ -0,0 +1,41 @@
+// void main() {
+//   String x = "1";
+//   int y = int.parse(x);
+//   y = y + 4;
+//   print(y);
+//   String z = y.toString();
+//      print(z);
+// } 
+// void main() {
+//   // List list = [10, 20.5, "Hello"];
+//   List<int> intList = [5, 10, 20];
+//   Set<String> s = {"Hello", "A"};
+//   int sum = 0;
+//   for (int i = 0; i < intList.length; i++) print(intList[i]);
+//   for (int i = 0; i < intList.length; i++) sum += intList[i];
+//   print("Sum: $sum");
+// } 
+// void main() {
+//   Map<String, int> myDetails = {
+//     "ID": 268,
+//     "Number": 3302,
+//     "CGPA": 3,
+//     "Reg": 1711,
+//   };
+//   print(myDetails['Reg']);
+//   print(myDetails);
+// } 
+// void main() {
+//   String value = "abcd";
+//   print(value.runes);
+//   print(value.runes.runtimeType);
+// } 
+void main() {
+  var a = 10;
+  a = 20;
+  var s = "Number";
+  print(a);
+  print(s);
+}
+
+//operators, string in dart,conditions and loops,practise q1 & q2 (assignment)
