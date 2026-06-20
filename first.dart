cf0594023df8d01dbb5f70b643a862043cd32c76
Neeladri Probal <neeladriprobal45@gmail.com>
2026-06-20 10:12:23 +0600

    Add first.dart

---
+++ first.dart
@@ -0,0 +1,18 @@
+// void main() {
+//   var name = "John";
+//   var last = "Doe";
+//   print("Full name is $name $last");
+// }
+
+// void main() {
+//   String name = "Neeladri Karmaker Probal";
+//   String id = "0182320012101268";
+//   double cgpa = 3.35;
+//   print("Name: $name\nId: $id\nCGPA: $cgpa");
+// }
+
+// void main() {
+//   double price = 21.214541434343;
+//   print(price.toStringAsFixed(3));
+// }
+
