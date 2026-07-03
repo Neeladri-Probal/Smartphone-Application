d8f9d4fe51ad156ce6bfbf8594c9d31905090acb
Neeladri Probal <neeladriprobal45@gmail.com>
2026-07-03 12:14:09 +0600

    thirdcls

---
+++ third.dart
@@ -0,0 +1,24 @@
+// import 'dart:io';
+
+// void main() {
+//   dynamic a = 10;
+//   a = "Probal";
+//   print(a);
+//   print(a.runtimeType);
+// }
+// import 'dart:io';
+
+// void main() {
+//   stdout.write("Enter input: ");
+//   String? input = stdin.readLineSync();
+//   int x = int.parse(stdin.readLineSync()!);
+//   stdout.write("Output: $input");
+//   print(x);
+// }
+//Example of compareTo()
+void main() { 
+   String item1 = "AD"; 
+   String item2 = "BC"; 
+   
+   print("Comparing item 1 with item 2: ${item1.compareTo(item2)}");
+} 
