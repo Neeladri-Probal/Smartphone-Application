0ae47fd332b007acf1e74c111b4523d8fe16194c
Neeladri Probal <neeladriprobal45@gmail.com>
2026-07-04 10:47:08 +0600

    substring, reverse, capitalize first letter of String, Ternary operator

--- fourth.dart
+++ fourth.dart
@@ -1,6 +1,36 @@
-import 'dart:io';
+// void main() {
+//   String text = "I love computer";
+//   print(text.substring(2));
+//   print(text.substring(2, 6));
+// }
 
+// void main() {
+//   String input = "hello";
+//   print(input.split('').reversed.join());
+// }
+
+//Example of capitalize first letter of String
+// void main() {
+//   String text = "hello world";
+//   String str = text[0].toUpperCase();
+//   String substr = text.substring(1);
+//   print(str + substr);
+// }
+// void main() {
+//   int num1 = 10;
+//   int num2 = 15;
+//   int max = 0;
+//   if (num1 > num2) {
+//     max = num1;
+//   } else {
+//     max = num2;
+//   }
+//   print("The greatest number is $max");
+//   int mx = num1 > num2 ? num1 : num2;
+//   print(mx);
+// }
 void main() {
-  String? s = stdin.readLineSync();
-  print(s);
+  int age = 10;
+  String tn = (age > 12 && age < 20) ? "Teenger" : "Not teeneger";
+  print(tn);
 }
